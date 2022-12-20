667555fac7108144ee25a61964a5c351401e0ec0
MarcoCalabretta1988 <calabretta.mar@gmail.com>
2022-12-20 19:13:05 +0100

    2 version inizialization

--- js/script.js
+++ js/script.js
@@ -8,86 +8,48 @@ const hoursElement = document.getElementById('hours');
 const minutesElement = document.getElementById('minutes');
 const secondsElement = document.getElementById('seconds');
 
-//Estrapolo data odierna ene estraggo valori
+//Recupero data odierna
  const now = new Date();
- const day = now.getDate();
- const hours = now.getHours();
- const minutes = now.getMinutes();
- const seconds = now.getSeconds();
- 
-let daysToChristmas = 24 - day;
-let hoursToChristmas = 23 - hours;
-let minutesToChristmas = 59 - minutes;
-let secondsToChristmas = 59 - seconds;
+
+//Recupero millisecondi al natale
+ const christmas = new Date (2022 , 11,25,0,0,0);
+ const christmasMillisecond = christmas.getTime();
+ console.log(christmasMillisecond);
+
+
+ // definisco parametri secondo i millisecondi
+  const second= 1000;
+  const minute = second * 60;
+  const hour = minute * 60;
+  const day = hour * 24; 
 
 
 
- //Inizializzo I valori del count down
- daysElement.innerText = daysToChristmas;
- hoursElement.innerText = hoursToChristmas;
- minutesElement.innerText = minutesToChristmas;
- secondsElement.innerText = secondsToChristmas;
-console.log(hours,minutes,seconds,day);
+  
+  //avvio funzione a tempo
+  const countDown = setInterval( () => {
+  
+    const nowMillisecond = now.getTime();
+    console.log(nowMillisecond);
 
-//avvio funzione a tempo
-const actualSecond = setInterval( () => {
+    const delta = christmasMillisecond - nowMillisecond;
 
-//controlli dei secondi---------------------------------------
-if (!secondsToChristmas){
+    const daysToChristmas = Math.floor(delta / day);
+    const hoursToChristmas = Math.floor((delta % day) / hour );
+    const minutesToChristmas = Math.floor((delta % hour) / minute );
+    const secondsToChristmas =Math.floor((delta % minute) / second );
 
-  secondsToChristmas = 59;
-  secondsElement.innerText = secondsToChristmas;
 
- 
-  //?controlli sui minuti-----------------------------------
-  if (!minutesToChristmas){
-
-    minutesToChristmas = 59;
+    
+    //Stampo i valori calcolati
+    daysElement.innerText = daysToChristmas;
+    hoursElement.innerText = hoursToChristmas;
     minutesElement.innerText = minutesToChristmas;
+    secondsElement.innerText = secondsToChristmas;
+    
 
-    // TODO controlli ore ---------------------------------
-    if (!hoursToChristmas){
-
-        hoursToChristmas = 23;
-        minutesElement.innerText = hoursToChristmas;
-        //! day controllers---------------------------------
-        if (daysToChristmas){
-            daysElement.innerText = --hoursToChristmas;
-          }
-        //!-------------------------------------------------
-      }
-      else if( hoursToChristmas <= 10){
-        hoursElement.innerText = '0' + --hoursToChristmas;
-      }
-      else{
-         
-        hoursElement.innerText = --hoursToChristmas;
-      }
-     //TODO ------------------------------------------------
-  }
-  else if( minutesToChristmas <= 10){
-    minutesElement.innerText = '0' + --minutesToChristmas;
-  }
-  else{
-     
-    minutesElement.innerText = --minutesToChristmas;
-  }
-  //?---------------------------------------------------------
-}
-else if( secondsToChristmas <= 10){
-    secondsElement.innerText = '0' + --secondsToChristmas;
-}
-else{
-   
-    secondsElement.innerText = --secondsToChristmas;
-}
-//-------------------------------------------------------------
-
-
-//COntrollo finale
-
-if (daysToChristmas === 0 && hoursToChristmas === 0 && minutesToChristmas === 0 && secondsToChristmas === 0 ){
-    clearInterval(actualSecond);
+if (delta < 0 ){
+    clearInterval(countDown);
 }
 }, 1000);
 
