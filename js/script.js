f8f747580fcd9030dd7697adf6ac2a91083e5605
MarcoCalabretta1988 <calabretta.mar@gmail.com>
2022-12-20 17:02:39 +0100

    Add firs version to improved

--- js/script.js
+++ js/script.js
@@ -15,10 +15,10 @@ const secondsElement = document.getElementById('seconds');
  const minutes = now.getMinutes();
  const seconds = now.getSeconds();
  
-const daysToChristmas = 24 - day;
-const hoursToChristmas = 23 - hours;
-const minutesToChristmas = 59 - minutes;
-const secondsToChristmas = 59 - seconds;
+let daysToChristmas = 24 - day;
+let hoursToChristmas = 23 - hours;
+let minutesToChristmas = 59 - minutes;
+let secondsToChristmas = 59 - seconds;
 
  //Inizializzo I valori del count down
  daysElement.innerText = daysToChristmas;
@@ -27,7 +27,67 @@ const secondsToChristmas = 59 - seconds;
  secondsElement.innerText = secondsToChristmas;
 console.log(hours,minutes,seconds,day);
 
+//avvio funzione a tempo
+const actualSecond = setInterval( () => {
 
+//controlli dei secondi---------------------------------------
+if (!secondsToChristmas){
+
+  secondsToChristmas = 59;
+  secondsElement.innerText = secondsToChristmas;
+
+ 
+  //?controlli sui minuti-----------------------------------
+  if (!minutesToChristmas){
+
+    minutesToChristmas = 59;
+    minutesElement.innerText = minutesToChristmas;
+
+    // TODO controlli ore ---------------------------------
+    if (!hoursToChristmas){
+
+        hoursToChristmas = 23;
+        minutesElement.innerText = hoursToChristmas;
+        //! day controllers---------------------------------
+        if (daysToChristmas){
+            daysElement.innerText = --hoursToChristmas;
+          }
+        //!-------------------------------------------------
+      }
+      else if( hoursToChristmas <= 10){
+        hoursElement.innerText = '0' + --hoursToChristmas;
+      }
+      else{
+         
+        hoursElement.innerText = --hoursToChristmas;
+      }
+     //TODO ------------------------------------------------
+  }
+  else if( minutesToChristmas <= 10){
+    minutesElement.innerText = '0' + --minutesToChristmas;
+  }
+  else{
+     
+    minutesElement.innerText = --minutesToChristmas;
+  }
+  //?---------------------------------------------------------
+}
+else if( secondsToChristmas <= 10){
+    secondsElement.innerText = '0' + --secondsToChristmas;
+}
+else{
+   
+    secondsElement.innerText = --secondsToChristmas;
+}
+//-------------------------------------------------------------
+
+
+//COntrollo finale
+
+if (daysToChristmas === 0 && hoursToChristmas === 0 && minutesToChristmas === 0 && secondsToChristmas === 0 ){
+    clearInterval(actualSecond);
+}
+}, 1000);
 
 
 
