ec4729cde5ef322ce4cedeb33d4059b28795dc64
MarcoCalabretta1988 <calabretta.mar@gmail.com>
2022-12-20 19:24:03 +0100

    End logic no bug find

--- js/script.js
+++ js/script.js
@@ -8,14 +8,9 @@ const hoursElement = document.getElementById('hours');
 const minutesElement = document.getElementById('minutes');
 const secondsElement = document.getElementById('seconds');
 
-//Recupero data odierna
- const now = new Date();
-
 //Recupero millisecondi al natale
  const christmas = new Date (2022 , 11,25,0,0,0);
  const christmasMillisecond = christmas.getTime();
- console.log(christmasMillisecond);
-
 
  // definisco parametri secondo i millisecondi
   const second= 1000;
@@ -23,35 +18,34 @@ const secondsElement = document.getElementById('seconds');
   const hour = minute * 60;
   const day = hour * 24; 
 
-
-
-  
   //avvio funzione a tempo
   const countDown = setInterval( () => {
-  
-    const nowMillisecond = now.getTime();
-    console.log(nowMillisecond);
 
+    //Recupero data odierna ei sui millisecondi
+    const now = new Date();
+    const nowMillisecond = now.getTime();
+
+    //calcolo differenza al natale
     const delta = christmasMillisecond - nowMillisecond;
 
+    //calcolo giorni,minuti
     const daysToChristmas = Math.floor(delta / day);
     const hoursToChristmas = Math.floor((delta % day) / hour );
     const minutesToChristmas = Math.floor((delta % hour) / minute );
-    const secondsToChristmas =Math.floor((delta % minute) / second );
+    const secondsToChristmas = Math.floor((delta % minute) / second );
 
-
-    
     //Stampo i valori calcolati
     daysElement.innerText = daysToChristmas;
     hoursElement.innerText = hoursToChristmas;
     minutesElement.innerText = minutesToChristmas;
     secondsElement.innerText = secondsToChristmas;
     
-
+//Controllo finale
 if (delta < 0 ){
     clearInterval(countDown);
 }
-}, 1000);
+
+},1000);
 
 
 
